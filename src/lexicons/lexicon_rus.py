4b34a63c6c2ba2d17d1830f6fe2e4fc25c75498f
Yaroslav <qustoo@yandex.ru>
2024-05-23 02:44:54 +0300

    update lexicons

--- src/lexicons/lexicon_rus.py
+++ src/lexicons/lexicon_rus.py
@@ -1,3 +1,7 @@
+LEXICON_COMMANDS_RU = {
+    '/start': 'Привет!\nЭтот бот умеет агрегировать информацию из различных источников!',
+    '/help': 'Привет!\nСкоро тут будет будет инфа по помощи!'
+}
 LEXICON_RUS = {
     'start': 'Начнем работу!',
     'help': 'Помощь',
@@ -13,8 +17,12 @@ LEXICON_RUS = {
     'years': 'Год выпуска',
     'pages': 'Количество страниц',
     'start_fill_based_parameters': 'Ввести данные для поиска',
-    'back_to_based_searching_keyboard': 'Вернуться к базовым параметрам',
     'run_aggregation_papers': 'Найти',
     'backward': '<<',
-    'forward': '>>'
+    'forward': '>>',
+    'add_to_bookmarks': 'Добавить статью в закладки',
+    'no_bookmarks': 'Не найдено ни одной сохраненной статьи',
+    'edit_bookmarks_button': '❌ РЕДАКТИРОВАТЬ',
+    'del': '❌',
+    'cancel_editing_bookmarks': 'Закладки убраны.'
 }

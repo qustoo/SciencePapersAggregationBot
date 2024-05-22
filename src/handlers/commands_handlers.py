450b826d53757595f773a4839c626f38c43f22cc
Yaroslav <qustoo@yandex.ru>
2024-05-23 02:45:28 +0300

    add bookmark command

--- src/handlers/commands_handlers.py
+++ src/handlers/commands_handlers.py
@@ -1,31 +1,39 @@
 from aiogram import F, Router
+from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardRemove
 
-from src.consts import HELP_MESSAGE, START_MESSAGE
-from src.keybords.service import (get_searching_parameters_keyboard,
+from src.database import AsyncBotDatabase
+from src.keybords.service import (create_bookmarks_keyboard,
+                                  get_searching_parameters_keyboard,
                                   get_started_keyboard)
-from src.lexicons.lexicon_rus import LEXICON_RUS
+from src.lexicons.lexicon_rus import LEXICON_COMMANDS_RU, LEXICON_RUS
 
 router = Router()
 
 
-# /start
-@router.message(F.text == '/start')
+@router.message(Command(commands='start'))
 async def start_command(message: Message):
-    await message.answer(text=START_MESSAGE, reply_markup=get_started_keyboard())
+    await message.answer(text=LEXICON_COMMANDS_RU['/start'], reply_markup=get_started_keyboard())
 
 
-# /help
-@router.message(F.text == LEXICON_RUS['help'])
+@router.message(Command(commands='help'))
 async def help_command(message: Message):
-    await message.answer(text=HELP_MESSAGE)
+    await message.answer(text=LEXICON_COMMANDS_RU['/help'])
 
 
-# base searching
-@router.message(F.text.in_({
-    LEXICON_RUS['start_fill_based_parameters'],
-    LEXICON_RUS['back_to_based_searching_keyboard']})
-)
+@router.message(Command(commands='bookmarks'))
+async def process_bookmarks_command(message: Message, database: AsyncBotDatabase):
+    bookmarks = await database.fetch_user_bookmarks_data(user_id=message.from_user.id)
+    if bookmarks:
+        await message.answer(
+            text='Найденные заметки:\n\n',
+            reply_markup=create_bookmarks_keyboard(*bookmarks)
+        )
+    else:
+        await message.answer(text=LEXICON_RUS['no_bookmarks'])
+
+
+@router.message(F.text == LEXICON_RUS['start_fill_based_parameters'])
 async def base_searching(message: Message):
     await message.answer(
         text='Начинаем!',

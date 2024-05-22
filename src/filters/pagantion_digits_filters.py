7f65d7d1d28e4d75d40dfc92debb1d6c5d8a168c
Yaroslav <qustoo@yandex.ru>
2024-05-23 02:43:56 +0300

    removed old files

--- src/filters/pagantion_digits_filters.py
+++
@@ -1,7 +0,0 @@
-from aiogram.filters import BaseFilter
-from aiogram.types import CallbackQuery
-
-
-class IsDigitCallbackData(BaseFilter):
-    async def __call__(self, callback: CallbackQuery) -> bool:
-        return callback.data.isdigit()
--- src/utils/states_enums.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class StatesName(Enum):
-    TERMS = 'terms'
-    SOURCE_PUBLISHED = 'source_published'
-    AUTHORS = 'authors'
-    YEARS = ('min_years', 'max_years')
-    PAGES = ('min_pages', 'max_pages')

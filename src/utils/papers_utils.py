73614ad65003fa233f541b02bd6915123192865d
Yaroslav <qustoo@yandex.ru>
2024-05-26 21:38:08 +0300

    add new insert data(type, page_count)

--- src/utils/papers_utils.py
+++ src/utils/papers_utils.py
@@ -10,11 +10,13 @@ async def add_papers_to_database(papers: list, database: AsyncBotDatabase, user_
                 'doi': paper.doi,
                 'title': paper.title,
                 'abstract': paper.abstract,
+                'type': paper.type,
                 'publication_date': paper.publication_date,
                 'cites': paper.cited_by_count,
                 'topic': paper.topic,
                 'authors': paper.authors_countries_info,
-                'sources': paper.sources
+                'sources': paper.sources,
+                'page_count': paper.biblio.page_count
             },
             user_id=user_id,
         )
